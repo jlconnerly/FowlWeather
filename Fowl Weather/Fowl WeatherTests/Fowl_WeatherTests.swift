096efe249f63b9a795abb14c561eefefefefe001
Jake <jlconnerly@gmail.com>
2020-05-16 23:40:43 -0500

    added testing enviornment

---
+++ Fowl Weather/Fowl WeatherTests/Fowl_WeatherTests.swift
@@ -0,0 +1,33 @@
+//
+//  Fowl_WeatherTests.swift
+//  Fowl WeatherTests
+//
+//  Created by Jake Connerly on 5/16/20.
+//  Copyright © 2020 jake connerly. All rights reserved.
+//
+
+import XCTest
+
+class Fowl_WeatherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
